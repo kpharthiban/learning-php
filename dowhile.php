aa59d34e6a2acf6735b2dbfd3bbcadf553b7fca3
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-03-04 17:16:07 +0800

    Do while loop

---
+++ dowhile.php
@@ -0,0 +1,13 @@
+<?php
+
+do {
+    // loop body
+    $diceRoll = rand(1,6);
+    echo "You rolled a $diceRoll\n";
+    if (6 == $diceRoll) {
+        echo "Congrats! You hit the jackpot! 🎉 \n";
+    }
+
+    echo "Roll again? (y/n)";
+    $rollAgain = trim(fgets(STDIN));
+} while ('y' == $rollAgain);
