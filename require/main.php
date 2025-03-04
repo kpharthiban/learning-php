ee631370dd44d118682bcc7d3f229b962e4c8649
Pharthiban Kumarhesan <pharthibankumarhesan@gmail.com>
2025-03-04 17:45:01 +0800

    require or include

---
+++ require/main.php
@@ -0,0 +1,7 @@
+<?php
+
+include "config.php"; 
+// use include or require (require ensures the file is there -> prevents running if file not found)
+// require_once or include_once can be used as well
+
+echo "Database $dbHost:$dbUser";
